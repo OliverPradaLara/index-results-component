297fdda74913ead90dd2fd8e45223515f02901b3
Harinson Arellan <harrinson.arellan@gmail.com>
2021-03-07 18:15:54 -0300

    Navbar listo

--- index-results-component/src/layout/Header.jsx
+++ index-results-component/src/layout/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Navbar, Nav, Row, Col } from 'react-bootstrap';
-import  indiceInnivacionGris  from './../SVG/icons/indice-innovacion-publica-gris.svg'
-import  laboratorioGobiernoGris  from './../SVG/icons/Laboratorio-Gobierno-gris.svg'
+import indiceInnivacionGris from './../SVG/icons/indice-innovacion-publica-gris.svg'
+import laboratorioGobiernoGris from './../SVG/icons/Laboratorio-Gobierno-gris.svg'
 
 export const Header = () => {
     return (
         <Row>
             <Col md={12}>
                 <Navbar bg="light" variant="light">
-                    <Col md={3}>
+                    <Col md={4}>
                         <Navbar.Brand href="#home" className="align-left">
-                        <img src = {laboratorioGobiernoGris}/>
-                        <img src = {indiceInnivacionGris}/>
-                            </Navbar.Brand>
+                            <Row>
+                                <Col md={4}>
+                                    <img width="100%" src={laboratorioGobiernoGris} />
+                                </Col>
+                                <Col md={4}>
+                                    <img width="100%" src={indiceInnivacionGris} />
+                                </Col>
+                            </Row>
+                        </Navbar.Brand>
                     </Col>
-                    <Col md={4}/>
+                    <Col md={3} />
                     <Col md={5}>
                         <Nav className="mr-auto">
                             <Nav.Link className="color-orange-one" href="#home"><b>Resultados del índice</b></Nav.Link>
