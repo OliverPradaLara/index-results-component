d844a9e1cfd6d872c86d70c12af0197c10a88bdc
OliverPradaLara <oprada.epsilon@gmail.com>
2021-03-08 22:06:24 -0300

    textos traducidos en objeto del hook del tab de dimensiones

--- index-results-component/src/i18n/en.js
+++ index-results-component/src/i18n/en.js
@@ -2,7 +2,23 @@ export const eng = {
     index_results: "Index Results",
     measurement_methodology: "Measurement Methodology",
     benefits_of_participating: "Benefits of participating",
-
-
-    dimensions: "Dimensions"
+    dimensions: "Dimensions",
+    tittleDimensions:"Average of each category of institutions for the 3 dimensions measured in the Public Innovation Index. The dimensions have a maximum score of 20, 40 and 40 points respectively, adding up to a maximum of 100.",
+    tittleInstitutionals:"INSTITUTIONAL RESOURCES",
+    tittleProcess:"PRACTICES AND PROCESSES",
+    tittleCollaboration:"COLLABORATION AND OPENING",
+    dimensionsInstitutionalsText:"It corresponds to the most basic or enabling aspects for the development of capacities to innovate. Without its existence, the following dimensions have a low probability of strengthening, hence their relevance. It considers elements related to human talent, the institutional framework, resources and digital infrastructure, together with elements of strategy and governance of innovation in each service.",
+    dimensionsProcessText:"It corresponds to the capacities necessary for the development of innovation initiatives. At a general level, the Index stimulates the development of initiatives that have achieved a high level of maturity, that is, ideally implemented and evaluated. In addition, it considers the degree of formalization of the procedures and innovation processes based on the logic of the design of services, that is, prototyping and testing for the early detection of failures.",
+    dimensionsCollaborationText:"It considers the ability of public services to interact with their environment, allowing them to learn and adapt to changing contexts. It seeks to identify collaborative work with users or beneficiaries, with other institutions within the sphere of",
+    dimensionsdetailsTittle:"see detail subdimensions",
+    dimensionsAnalysisText:"See analysis",
+    dimensionsInformationPlus:"+ more information about dimensions",
+    collaborationUserTittle: "USER PARTICIPATION",
+    collaborationCoordinationTittle:"INTERINSTITUTIONAL COORDINATION",
+    collaborationManagmentTittle: "ENVIRONMENT MANAGEMENT",
+    collaborationUserText: "It encourages the use of clear and inclusive language in communications and channels, promotes participation mechanisms, and considers the involvement of users of each entity in the various phases of innovation, such as: participation in the identification of gaps through research processes, participation as co-creators of solutions, participation as evaluators of solutions, etc.",
+    collaborationCoordinationText:"It seeks to measure collaboration and eventual interoperability between services. Specifically, it encourages the participation of different services in collaborative processes of identification of opportunities to innovate, co-creation of solutions, implementation and / or joint evaluation.",
+    collaborationManagmentText:"It measures the existence of spaces to collaborate with organizations that are outside the sphere of the State, which may be entities of civil society or the private sector, in order to encourage the incorporation of learning from an open innovation perspective."
 }
+
+
