d8f3912196238737b30754d3bb632c8a50a2a9b3
Harinson Arellan <harrinson.arellan@gmail.com>
2021-03-16 16:25:01 -0300

    lo ultimo

--- src/views/DimensionsTab.jsx
+++ src/views/DimensionsTab.jsx
@@ -19,13 +19,11 @@ export const DimensionsTab = (props) => {
                 ) : (
                     <>
                         <Row>
-                            <Col md={1} />
-                            <Col md={10}>
+                            <Col md={12}>
                                 <div className="p-4 mt-4">
                                     <p className="text-descripcion">{t("tittleDimensions")}</p>
                                 </div>
                             </Col>
-                            <Col md={1} />
                         </Row>
                         <Row>
                             <Col md={12}>
@@ -34,55 +32,61 @@ export const DimensionsTab = (props) => {
                                 </div>
                             </Col>
                         </Row>
-                        <Row>
-                            <Row className="p-5 box-dimensions-text">
-                                <Col md={4}>
-                                    <Card border="0">
-                                        <Card.Body className="p-5">
-                                            <Card.Title className="title-resultado">{t("tittleInstitutionals")}</Card.Title>
-                                            <Card.Text className="descripcion-resultado">
-                                                {t("dimensionsInstitutionalsText")}
-                                            </Card.Text>
-                                            <div className="text-center">
-                                                <a onClick={() => setView(1)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col md={4}>
-                                    <Card border="0">
-                                        <Card.Body className="borde p-5">
-                                            <Card.Title className="title-resultado">{t("tittleProcess")}</Card.Title>
-                                            <Card.Text className="descripcion-resultado">
-                                                {t("dimensionsProcessText")}
-                                            </Card.Text>
-                                            <div className="text-center">
-                                                <a onClick={() => setView(2)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col md={4}>
-                                    <Card border="0" >
-                                        <Card.Body className="p-5">
-                                            <Card.Title className="title-resultado">{t("tittleCollaboration")}</Card.Title>
-                                            <Card.Text className="descripcion-resultado">
-                                                {t("dimensionsCollaborationText")}
-                                            </Card.Text>
-                                            <div className="text-center">
-                                                <a onClick={() => setView(3)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            </Row>
-                        </Row>
-                        <Row>
-                            <Col md={12} className="text-center pt-4 pb-4">
-                                <a href="https://www.google.com/?hl=es" className="color-orange-one">{t("dimensionsInformationPlus")} </a>
+                        <Row className=" box-dimensions-text">
+                            <Col md={4} className="p-5">
+                                <Card border="0">
+                                    <Card.Body>
+                                        <Card.Title className="title-resultado">{t("tittleInstitutionals")}</Card.Title>
+                                        <Card.Text className="descripcion-resultado">
+                                            {t("dimensionsInstitutionalsText")}
+                                        </Card.Text>
+                                        {/* <div className="text-center">
+                                            <a onClick={() => setView(1)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                                        </div> */}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4} className="p-5">
+                                <Card border="0">
+                                    <Card.Body>
+                                        <Card.Title className="title-resultado">{t("tittleProcess")}</Card.Title>
+                                        <Card.Text className="descripcion-resultado">
+                                            {t("dimensionsProcessText")}
+                                        </Card.Text>
+                                        {/* <div className="text-center mb-5">
+                                            <a onClick={() => setView(2)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                                        </div> */}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4} className="p-5">
+                                <Card border="0" >
+                                    <Card.Body >
+                                        <Card.Title className="title-resultado">{t("tittleCollaboration")}</Card.Title>
+                                        <Card.Text className="descripcion-resultado">
+                                            {t("dimensionsCollaborationText")}
+                                        </Card.Text>
+                                        {/* <div className="text-center">
+                                            <a onClick={() => setView(3)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                                        </div> */}
+                                    </Card.Body>
+                                </Card>
                             </Col>
                         </Row>
+                        <Row>
+                        </Row>
+                        <Row className="text-center subir">
+                            <Col md={4} className="text-center p-5">
+                                <a onClick={() => setView(1)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                            </Col>
+                            <Col md={4} className="text-center p-5">
+                                <a onClick={() => setView(2)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                            </Col>
+                            <Col md={4} className="text-center p-5">
+                                <a onClick={() => setView(3)} className="color-orange-one">{t("dimensionsdetailsTittle")}</a>
+                            </Col>
 
+                        </Row>
                     </>
                 )
             }
