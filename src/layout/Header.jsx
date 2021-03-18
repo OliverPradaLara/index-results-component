c9f3b21e15bb2b699d11702025c533e26601b972
Nicolas Davila <nicolas@patagonia-it.com>
2021-03-18 18:44:59 -0300

    WIP: Navbar Responsivo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
     const { t, i18n } = useTranslation();
     return (
         <>
-            <Navbar className="pt-4 pb-4" bg="light" variant="light">
+            <Navbar expand="lg" collapseOnSelect className="pt-4 pb-4" bg="light" variant="light">
                 <Row className="row-navbar">
                     <Col md={6}>
                         <Navbar.Brand href="#home" className="align-left">
@@ -23,32 +23,37 @@ export const Header = () => {
                             </Row>
                         </Navbar.Brand>
                     </Col>
-                    <div className="col-md-6 d-flex justify-content-end">
-                        <Nav className="d-flex justify-content-end">
-                            <Nav.Item>
-                                <Nav.Link className="color-orange-one text-center text_decoration" href="#home"><b>{t("index_results")}</b></Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-center" href="#features"><b>{t("measurement_methodology")}</b></Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-center" href="#pricing"><b>{t("benefits_of_participating")} </b></Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-center" href="#category"><b>{t("institution_categories")}</b></Nav.Link>
-                            </Nav.Item>
-                        <NavDropdown className="color-nav posicion-class" title={language ? `| ESP` : "| ENG"} id="basic-nav-dropdown">
-                                <NavDropdown.Item onClick={() => {
-                                    setLanguage(true)
-                                    i18n.changeLanguage("es")
-                                }}>{t("spanish")}</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => {
-                                    setLanguage(false)
-                                    i18n.changeLanguage("en")
-                                }}>{t("english")}</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </div>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Col md={6} className="d-flex justify-content-end">
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="d-flex justify-content-end">
+                                <Nav.Item>
+                                    <Nav.Link className="color-orange-one text-center text_decoration" href="#home"><b>{t("index_results")}</b></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-center" href="#features"><b>{t("measurement_methodology")}</b></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-center" href="#pricing"><b>{t("benefits_of_participating")} </b></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-center" href="#category"><b>{t("institution_categories")}</b></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className="center-translation">
+                                    <NavDropdown className="color-nav posicion-class" title={language ? `| ESP` : "| ENG"} id="basic-nav-dropdown">
+                                        <NavDropdown.Item onClick={() => {
+                                            setLanguage(true)
+                                            i18n.changeLanguage("es")
+                                        }}>{t("spanish")}</NavDropdown.Item>
+                                        <NavDropdown.Item onClick={() => {
+                                            setLanguage(false)
+                                            i18n.changeLanguage("en")
+                                        }}>{t("english")}</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav.Item>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Col>
                 </Row>
             </Navbar>
 
