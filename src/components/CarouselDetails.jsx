fcd63eb8217aa0073b066badce827351ca19a4d6
Nicolas Davila <nicolas@patagonia-it.com>
2021-03-26 16:53:06 -0300

    carruseles card

--- src/components/CarouselDetails.jsx
+++ src/components/CarouselDetails.jsx
@@ -54,50 +54,76 @@ export const CarouselDetails = (props) => {
                                 </Col>
                             </Row>
                             <Row className="interpretation-card">
-                                <Col md={4}>
-                                    <Card border="0">
-                                        <Card.Body className="interpretation-card">
+                                <Col md={4} className="interpretation-card dimensions-cards">
+                                    <Row className="row-a-dimensions-top">
+                                        <Col>
                                             <Card.Title className="title-resultado">{t("title_Human_Talent")}</Card.Title>
                                             <Card.Text className="descripcion-resultado">
                                                 {t("description_Human_Talent")}
                                             </Card.Text>
-                                            <a onClick={() => { setDetailView(11) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                        </Card.Body>
-                                    </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row className="row-a-dimensions-bottom">
+                                        <Col className="col-a-dimensions">
+                                            <div className="text-center">
+                                                <a onClick={() => { setDetailView(11) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                            </div>
+                                        </Col>
+                                    </Row>
                                 </Col>
-                                <Col md={4}>
-                                    <Card border="0">
-                                        <Card.Body className="interpretation-card">
+
+                                <Col md={4} className="interpretation-card dimensions-cards">
+                                    <Row className="row-a-dimensions-top">
+                                        <Col>
                                             <Card.Title className="title-resultado">{t("title_Institutional_Frame")}</Card.Title>
                                             <Card.Text className="descripcion-resultado">
                                                 {t("description_Institutional_Frame")}
                                             </Card.Text>
-                                            <a onClick={() => { setDetailView(12) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                        </Card.Body>
-                                    </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row className="row-a-dimensions-bottom">
+                                        <Col className="col-a-dimensions">
+                                            <div className="text-center">
+                                                <a onClick={() => { setDetailView(12) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                            </div>
+                                        </Col>
+                                    </Row>
                                 </Col>
-                                <Col md={4}>
-                                    <Card border="0">
-                                        <Card.Body className="interpretation-card">
+
+                                <Col md={4} className="interpretation-card dimensions-cards">
+                                    <Row className="row-a-dimensions-top">
+                                        <Col>
                                             <Card.Title className="title-resultado">{t("title_Digital_Resources")}</Card.Title>
                                             <Card.Text className="descripcion-resultado">
                                                 {t("description_Digital_Resources")}
                                             </Card.Text>
-                                            <a onClick={() => { setDetailView(13) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                        </Card.Body>
-                                    </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row className="row-a-dimensions-bottom">
+                                        <Col className="col-a-dimensions">
+                                            <div className="text-center">
+                                                <a onClick={() => { setDetailView(13) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                            </div>
+                                        </Col>
+                                    </Row>
                                 </Col>
-                                <Col md={4}>
-                                    <Card border="0" >
-                                        <Card.Body className="interpretation-card">
+
+                                <Col md={4} className="interpretation-card dimensions-cards">
+                                    <Row className="row-a-dimensions-top">
+                                        <Col>
                                             <Card.Title className="title-resultado">{t("title_Strategy_and_Governability")}</Card.Title>
                                             <Card.Text className="descripcion-resultado">
                                                 {t("description_Strategy_and_Governability")}
                                             </Card.Text>
-                                            <a onClick={() => { setDetailView(14) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-
-                                        </Card.Body>
-                                    </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row className="row-a-dimensions-bottom">
+                                        <Col className="col-a-dimensions">
+                                            <div className="text-center">
+                                                <a onClick={() => { setDetailView(14) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                            </div>
+                                        </Col>
+                                    </Row>
                                 </Col>
                             </Row>
                             <Row>
@@ -113,6 +139,9 @@ export const CarouselDetails = (props) => {
                                 </Col>
                             </Row>
                         </Carousel.Item>
+
+
+
                         <Carousel.Item>
                             <Row className="d-flex color-grey justify-content-center">
                                 <Col md={10} className>
@@ -132,40 +161,58 @@ export const CarouselDetails = (props) => {
                             </Row>
                             <Row>
                                 <Row className="interpretation-card">
-
-                                    <Col md={4}>
-                                        <Card border="0">
-                                            <Card.Body className="interpretation-card">
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">{t("title_Activities_or_practices")}</Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("description_Activities_or_practices")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(21) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(21) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
-                                    <Col md={4}>
-                                        <Card border="0">
-                                            <Card.Body className="interpretation-card">
+
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">{t("title_Proceedings")}</Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("description_Proceedings")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(22) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(22) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
-                                    <Col md={4}>
-                                        <Card border="0" >
-                                            <Card.Body className="interpretation-card">
+
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">{t("title_Processes")}</Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("description_Processes")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(23) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(23) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
                                 </Row>
                             </Row>
@@ -182,6 +229,9 @@ export const CarouselDetails = (props) => {
                                 </Col>
                             </Row>
                         </Carousel.Item>
+
+
+
                         <Carousel.Item>
                             <Row className="d-flex color-grey justify-content-center">
                                 <Col md={10}>
@@ -200,42 +250,60 @@ export const CarouselDetails = (props) => {
                             </Row>
                             <Row>
                                 <Row className="interpretation-card">
-
-                                    <Col md={4}>
-                                        <Card border="0">
-                                            <Card.Body className="interpretation-card">
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">{t("collaborationUserTittle")}</Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("collaborationUserText")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(31) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(31) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
-                                    <Col md={4}>
-                                        <Card border="0">
-                                            <Card.Body className="interpretation-card">
+
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">
                                                     {t("collaborationCoordinationTittle")}
                                                 </Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("collaborationCoordinationText")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(32) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(32) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
-                                    <Col md={4}>
-                                        <Card border="0" >
-                                            <Card.Body className="interpretation-card">
+
+                                    <Col md={4} className="interpretation-card dimensions-cards">
+                                        <Row className="row-a-dimensions-top">
+                                            <Col>
                                                 <Card.Title className="title-resultado">{t("collaborationManagmentTittle")}</Card.Title>
                                                 <Card.Text className="descripcion-resultado">
                                                     {t("collaborationManagmentText")}
                                                 </Card.Text>
-                                                <a onClick={() => { setDetailView(33) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
-
-                                            </Card.Body>
-                                        </Card>
+                                            </Col>
+                                        </Row>
+                                        <Row className="row-a-dimensions-bottom">
+                                            <Col className="col-a-dimensions">
+                                                <div className="text-center">
+                                                    <a onClick={() => { setDetailView(33) }} className="color-orange-one">{t("dimensionsAnalysisText")}</a>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </Col>
                                 </Row>
                             </Row>
