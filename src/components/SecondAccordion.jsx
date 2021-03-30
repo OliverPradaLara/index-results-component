aa2208f662c2affeb2148564904ac36a6d82cada
Nicolas Davila <nicolas@patagonia-it.com>
2021-03-30 17:39:24 -0300

    svg nombres gris

--- src/components/SecondAccordion.jsx
+++ src/components/SecondAccordion.jsx
@@ -3,6 +3,23 @@ import { Accordion, Card, Row, Col } from 'react-bootstrap'
 import Fijo2 from "../SVG/img/Fijo2.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslation } from 'react-i18next'
+import Grupo1 from "../SVG/img/Grupo1.svg"
+import Grupo2 from "../SVG/img/Grupo2.svg"
+import Grupo3 from "../SVG/img/Grupo3.svg"
+import Grupo4 from "../SVG/img/Grupo4.svg"
+import Grupo5 from "../SVG/img/Grupo5.svg"
+import Grupo6 from "../SVG/img/Grupo6.svg"
+import Grupo7 from "../SVG/img/Grupo7.svg"
+import Grupo8 from "../SVG/img/Grupo8.svg"
+import Grupo9 from "../SVG/img/Grupo9.svg"
+import Grupo10 from "../SVG/img/Grupo10.svg"
+import Grupo11 from "../SVG/img/Grupo11.svg"
+import Grupo12 from "../SVG/img/Grupo12.svg"
+import Grupo13 from "../SVG/img/Grupo13.svg"
+import Grupo14 from "../SVG/img/Grupo14.svg"
+import Grupo15 from "../SVG/img/Grupo15.svg"
+import Grupo16 from "../SVG/img/Grupo16.svg"
+import Grupo17 from "../SVG/img/Grupo17.svg"
 
 export const SecondAccordion = () => {
     const [drop, setDrop] = useState(true)
@@ -53,16 +70,60 @@ export const SecondAccordion = () => {
             </Accordion.Toggle>
 
             <Accordion.Collapse className="size-box" eventKey="1">
-                <Row className="accordion2ContainerCollapse pb-4">
-                    <Col md={4} className="accordion2Box">
-                        <h1 className="accordion2Tittle">130</h1>
-                        <h2 className="accordion2SubTittle">{t("innovation_initiatives")}</h2>
-                        <p className="accordion2Body">{t("text_acc_2")}</p>
-                    </Col>
-                    <Col md={8} className="text-center img-tamano">
-                        <img src={Fijo2} alt="gráfico" className="fijo2Style" />
-                    </Col>
-                </Row>
+                <>
+                    <Row className="accordion2ContainerCollapse pb-4">
+                        <Col md={4} className="accordion2Box">
+                            <h1 className="accordion2Tittle">130</h1>
+                            <h2 className="accordion2SubTittle">{t("innovation_initiatives")}</h2>
+                            <p className="accordion2Body">{t("text_acc_2")}</p>
+                        </Col>
+                        <Col md={8} className="text-center img-tamano">
+                            <img src={Fijo2} alt="gráfico" className="fijo2Style" />
+                        </Col>
+                    </Row>
+                    <Row>
+
+                        <h1> acordeon 3 responsivo</h1>
+                        <img src={Grupo1} alt="gráfico" className="fijo2Style" />
+
+                        <h1>Dimensiones 1</h1>
+                        <img src={Grupo2} alt="gráfico" className="fijo2Style" />
+                        <h1> Dimensiones 2</h1>
+                        <img src={Grupo3} alt="gráfico" className="fijo2Style" />
+                        <h1> Dimensiones 3</h1>
+                        <img src={Grupo7} alt="gráfico" className="fijo2Style" />
+                        
+                        <h1> General 1</h1>
+                        <img src={Grupo4} alt="gráfico" className="fijo2Style" />
+                        <h1> General 2</h1>
+                        <img src={Grupo5} alt="gráfico" className="fijo2Style" />
+                        <h1> General 3</h1>
+                        <img src={Grupo6} alt="gráfico" className="fijo2Style" />
+                        
+                        <h1> Talento Humano</h1>
+                        <img src={Grupo8} alt="gráfico" className="fijo2Style" />
+                        <h1> Marco institucional</h1>
+                        <img src={Grupo9} alt="gráfico" className="fijo2Style" />
+                        <h1> Recursos digitales</h1>
+                        <img src={Grupo10} alt="gráfico" className="fijo2Style" />
+                        <h1> Estrategia y gobernanza</h1>
+                        <img src={Grupo11} alt="gráfico" className="fijo2Style" />
+                        
+                        <h1> Actividades o practicas</h1>
+                        <img src={Grupo12} alt="gráfico" className="fijo2Style" />
+                        <h1> Procedimientos</h1>
+                        <img src={Grupo13} alt="gráfico" className="fijo2Style" />
+                        <h1> Procesos</h1>
+                        <img src={Grupo14} alt="gráfico" className="fijo2Style" />
+
+                        <h1> Participacion de usuarios</h1>
+                        <img src={Grupo15} alt="gráfico" className="fijo2Style" />
+                        <h1> Coordinacion interinstitucional</h1>
+                        <img src={Grupo16} alt="gráfico" className="fijo2Style" />
+                        <h1> Gestion del entorno</h1>
+                        <img src={Grupo17} alt="gráfico" className="fijo2Style" />
+                    </Row>
+                </>
             </Accordion.Collapse>
         </>
     )
