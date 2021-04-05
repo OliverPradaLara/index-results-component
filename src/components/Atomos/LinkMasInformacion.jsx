d6fdd462e355d8bd81a30d9287f5c09a8afe1bcd
Harinson Arellan <harrinson.arellan@gmail.com>
2021-04-05 13:00:56 -0400

    cambios

--- src/components/Atomos/LinkMasInformacion.jsx
+++ src/components/Atomos/LinkMasInformacion.jsx
@@ -2,7 +2,7 @@
 
 const LinkMasInformacion = ({link, texto}) => (
     <div  className="link-informacion">
-        <a href={link} className="color-orange-one"><i class="fas fa-plus mr-1"></i>{ texto }</a>
+        <a href={link} className="color-orange-one"><i className="fas fa-plus mr-1"></i>{ texto }</a>
     </div>
 )
 
